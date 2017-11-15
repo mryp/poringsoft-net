5ebf856c36c045b72d790e68a11358d9a8ab06a0
MRY <mry@poringsoft.net>
2017-11-15 22:31:06 +0900

    マテリアルデザインメニュー作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  title = 'PORING SOFT .NET';
+  widthTablet = 768;
+
+  @ViewChild('sidenav') sidenav: MatSidenavModule;
+  sidenaviMode = 'over';
+  sidenaviOpened = false;
+  sidenaviIconVisible = true;
+
+  naviLinkList = [
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+    { title: 'メニューアイテム', link: '/page', },
+  ];
+
+  ngOnInit() {
+    this.setSidenavi(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setSidenavi(event.target.innerWidth);
+  }
+
+  setSidenavi(windowSize: number) {
+    if (windowSize < this.widthTablet) {
+      this.sidenaviMode = 'over';
+      this.sidenaviOpened = false;
+      this.sidenaviIconVisible = true;
+    } else {
+      this.sidenaviMode = 'side';
+      this.sidenaviOpened = true;
+      this.sidenaviIconVisible = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,
+   MatSidenavModule,
+   MatCardModule,
+   MatIconModule,
+   MatListModule,
+   MatButtonModule
+  } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
